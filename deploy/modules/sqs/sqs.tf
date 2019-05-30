7d174af4ca0f7939090532411c73a2b3bb0116af
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-30 19:00:51 +0100

    adding dead letter queue to sqs

--- deploy/modules/sqs/sqs.tf
+++ deploy/modules/sqs/sqs.tf
@@ -1,8 +1,16 @@
 resource "aws_sqs_queue" "sqs" {
   name                      = "${var.name}"
-  delay_seconds             = 0
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  visibility_timeout_seconds = 150
+  redrive_policy = <<EOF
+  {
+    "maxReceiveCount": "3",
+    "deadLetterTargetArn": "${aws_sqs_queue.sqs_dead_letter_queue.arn}"
+  }
+  EOF
+}
+
+resource "aws_sqs_queue" "sqs_dead_letter_queue" {
+  name                      = "${var.name}-dead-letter-queue"
   visibility_timeout_seconds = 150
 }
+
